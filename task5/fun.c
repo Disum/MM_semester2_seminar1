52b37aafb2df47ff6687f03cd0792eb9c475ee67
yakov <mirenkovye@yandex.ru>
2020-02-14 06:40:29 +0300

    Fixed algorithms

--- task5/fun.c
+++ task5/fun.c
@@ -2,68 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include "fun.h"
-char *strstr_spcf(const char *str1, const char *str2);
 #define MAX_LEN 1024
-
-/* str1, str2 - нуль-терминированные строки. */
-char *strstr_spcf(const char *str1, const char *str2)
-{
-	char *c1, *c1_beg, *c2;
-
-	if( str2[0]=='^' )
-	{
-		c1 = (char *)str1;
-		c2 = (char *)str2 + 1;
-
-		if( *c2=='\0' )
-			return (char *)str1;
-
-		while( *c1==*c2 )
-		{
-			c1++;
-			c2++;
-
-			if( *c2=='\0' )
-				return (char *)str1;
-			if( *c1=='\0')
-				break;
-		}
-
-		return NULL;
-	}
-
-	for( c1_beg = (char *)str1, c1 = c1_beg; *c1_beg!='\0'; c1_beg++, c1 = c1_beg )
-	{
-		c2 = (char *)str2;
-
-		if( *c1=='^' || *c1=='\\' )
-		{
-			if( *c2!='\\' )
-				continue;
-			c2++;
-		}
-
-		while( *c1==*c2 )
-		{
-			c1++;
-			c2++;
-
-			if( *c2=='\0' )
-				return c1_beg;
-			if( *c1=='\0' )
-				break;
-
-			if( *c1=='^' || *c1=='\\' )
-			{
-				if( *c2!='\\' )
-					break;
-				c2++;
-			}
-		}
-	}
-
-	return NULL;
-}
 
 /* Ф-ия выводит в name_out строки из name_in, в которые входит s.
  * Символ '^' в начале s соответствует началу строки из name_in.
@@ -76,8 +15,8 @@ char *strstr_spcf(const char *str1, const char *str2)
 int fun(const char *name_in, const char *name_out, const char *s)
 {
 	FILE *in, *out;
-	int count = 0;
-	char *buff, *c;
+	int count = 0, s_sz, new_s_sz, i;
+	char *buff, *c, *new_s;
 
 	if( !(in = fopen(name_in, "r")) )
 		return -1;
@@ -94,21 +33,72 @@ int fun(const char *name_in, const char *name_out, const char *s)
 		return -3;
 	}
 
-	for( c = (char *)s; *c!='\0' && *c!='\n'; c++ ) { }
-	*c = '\0';
+	for( s_sz = 0; s[s_sz]!='\0' && s[s_sz]!='\n'; s_sz++ ) { }
 
-	while( fgets(buff, MAX_LEN, in) )
+	if( !(new_s = (char *)malloc(s_sz)) )
 	{
-		for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
-		*c = '\0';
+		free(buff);
+		fclose(in);
+		fclose(out);
+		return -3;
+	}
 
-		if( strstr_spcf(buff, s) )
+	new_s_sz = 0;
+	for( i = 0; i<s_sz; i++ )
+	{
+		if( s[i]=='\\' && (i + 1)<s_sz )
 		{
-			count++;
-			fprintf(out, "%s\n", buff);
+			switch( s[i + 1] )
+			{
+				case '^':
+					new_s[new_s_sz] = '^';
+					new_s_sz++;
+					i++;
+					continue;
+				case '\\':
+					new_s[new_s_sz] = '\\';
+					new_s_sz++;
+					i++;
+					continue;
+			}
+		}
+
+		new_s[new_s_sz] = s[i];
+		new_s_sz++;
+	}
+	new_s[new_s_sz] = '\0';
+
+	if( new_s[0]=='^' )
+	{
+		while( fgets(buff, MAX_LEN, in) )
+		{
+			for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
+			*c = '\0';
+
+			if( !strncmp(buff, new_s + 1, new_s_sz - 1) )
+			{
+				count++;
+				fprintf(out, "%s\n", buff);
+			}
+		}
+
+	}
+	else
+	{
+		while( fgets(buff, MAX_LEN, in) )
+		{
+			for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
+			*c = '\0';
+
+			if( strstr(buff, new_s) )
+			{
+				count++;
+				fprintf(out, "%s\n", buff);
+			}
 		}
 	}
 
+	free(new_s);
 	free(buff);
 	fclose(in);
 	fclose(out);
--- task6/fun.c
+++ task6/fun.c
@@ -2,76 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 #include "fun.h"
-char *strstr_spcf(const char *str1, const char *str2);
 #define MAX_LEN 1024
 
-/* str1, str2 - нуль-терминированные строки. */
-char *strstr_spcf(const char *str1, const char *str2)
-{
-	char *c1, *c1_beg, *c2;
-	int i, str1_size, str2_size;
-
-	for( str2_size = 0; str2[str2_size]!='\0'; str2_size++ ) { }
-
-	if( str2_size!=0 && str2[str2_size - 1]=='$' )
-	{
-		for( str1_size = 0; str1[str1_size]!='\0'; str1_size++ ) { }
-
-		for( i = str2_size - 2, c1 = (char *)str1 + str1_size - 1; i>=0; i--, c1-- )
-		{
-			if( str2[i]!=*c1 )
-				return NULL;
-		}
-
-		return c1;
-	}
-
-	for( c1_beg = (char *)str1, c1 = c1_beg; *c1_beg!='\0'; c1_beg++, c1 = c1_beg )
-	{
-		c2 = (char *)str2;
-
-		if( *c1=='$' || *c1=='\\' )
-		{
-			if( *c2!='\\' )
-				continue;
-			c2++;
-		}
-
-		while( *c1==*c2 )
-		{
-			c1++;
-			c2++;
-
-			if( *c2=='\0' )
-				return c1_beg;
-			if( *c1=='\0' )
-				break;
-
-			if( *c1=='$' || *c1=='\\' )
-			{
-				if( *c2!='\\' )
-					break;
-				c2++;
-			}
-		}
-	}
-
-	return NULL;
-}
-
 /* Ф-ия выводит в name_out строки из name_in, в которые входит s.
- * Символ '$' в конце s соответствует концу строки из name_in.
- * Символы '$' и '\' из name_in соответствуют '\$' и '\\' в s.
+ * Символ '^' в начале s соответствует началу строки из name_in.
+ * Символы '^' и '\' из name_in соответствуют '\^' и '\\' в s.
  * Ф-ия возвращает:
  * -1, если не удалось открыть name_in;
  * -2, если не удалось открыть name_out;
  * -3, если не удалось выделиь память;
- * кол-во выведенных строк, если проблем не обнаружено. */
+ * кол-во измененных строк, если проблем не возникло. */
 int fun(const char *name_in, const char *name_out, const char *s)
 {
 	FILE *in, *out;
-	int count = 0;
-	char *buff, *c;
+	int count = 0, s_sz, new_s_sz, buff_sz, i;
+	char *buff, *c, *new_s;
 
 	if( !(in = fopen(name_in, "r")) )
 		return -1;
@@ -88,21 +33,97 @@ int fun(const char *name_in, const char *name_out, const char *s)
 		return -3;
 	}
 
-	for( c = (char *)s; *c!='\0' && *c!='\n'; c++ ) { }
-	*c = '\0';
+	for( s_sz = 0; s[s_sz]!='\0' && s[s_sz]!='\n'; s_sz++ ) { }
 
-	while( fgets(buff, MAX_LEN, in) )
+	if( !(new_s = (char *)malloc(s_sz)) )
 	{
-		for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
-		*c = '\0';
+		free(buff);
+		fclose(in);
+		fclose(out);
+		return -3;
+	}
 
-		if( strstr_spcf(buff, s) )
+	new_s_sz = 0;
+	if( s[s_sz - 1]=='$' )
+	{
+		for( i = 0; i<(s_sz - 1); i++ )
 		{
-			count++;
-			fprintf(out, "%s\n", buff);
+			if( s[i]=='\\' && (i + 1)<s_sz )
+			{
+				switch( s[i + 1] )
+				{
+					case '$':
+						new_s[new_s_sz] = '$';
+						new_s_sz++;
+						i++;
+						continue;
+					case '\\':
+						new_s[new_s_sz] = '\\';
+						new_s_sz++;
+						i++;
+						continue;
+				}
+			}
+
+			new_s[new_s_sz] = s[i];
+			new_s_sz++;
+		}
+		new_s[new_s_sz] = '\0';
+
+		while( fgets(buff, MAX_LEN, in) )
+		{
+			for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
+			*c = '\0';
+
+			buff_sz = strlen(buff);
+			if( !strncmp(buff + buff_sz - new_s_sz, new_s, new_s_sz) )
+			{
+				count++;
+				fprintf(out, "%s\n", buff);
+			}
+		}
+
+	}
+	else
+	{
+		for( i = 0; i<s_sz; i++ )
+		{
+			if( s[i]=='\\' && (i + 1)<s_sz )
+			{
+				switch( s[i + 1] )
+				{
+					case '$':
+						new_s[new_s_sz] = '$';
+						new_s_sz++;
+						i++;
+						continue;
+					case '\\':
+						new_s[new_s_sz] = '\\';
+						new_s_sz++;
+						i++;
+						continue;
+				}
+			}
+
+			new_s[new_s_sz] = s[i];
+			new_s_sz++;
+		}
+		new_s[new_s_sz] = '\0';
+
+		while( fgets(buff, MAX_LEN, in) )
+		{
+			for( c = buff; *c!='\0' && *c!='\n'; c++ ) { }
+			*c = '\0';
+
+			if( strstr(buff, new_s) )
+			{
+				count++;
+				fprintf(out, "%s\n", buff);
+			}
 		}
 	}
 
+	free(new_s);
 	free(buff);
 	fclose(in);
 	fclose(out);
